d522efdc665c2e0d4837e10d55631c252c8b38d6
acgnfun <3348544562@qq.com>
2024-02-26 20:05:25 +0800

    修改项目文件

---
+++ README.md
@@ -0,0 +1,9 @@
+# libafutil
+
+Refactored from AFComponents with support for C/C++, x86, x64.
+
+IPC supports standard streams.
+
+## Thanks
+
+- [libcurl](https://github.com/curl/curl) Provides the ability to download from the web.
--- curl-init.bat
+++ curl-init.bat
@@ -1,31 +1,8 @@
 @echo off
-
-set CURL_VERSION=8.6.0
-set CURL_TAG=curl-8_6_0
-
-echo Initializing curl-%CURL_VERSION%.
-
 if not exist libafutil.sln (
-	echo Initialization failed: The working directory is incorrect, and the repository root directory should be used as the working directory.
+	echo Folder error.
 	exit 1
 )
-
-where git >nul 2>nul
-if %ERRORLEVEL% neq 0 (
-	echo Initialization failed: The git command is required, please install Git.
-	exit 1
-)
-
 rmdir /s /q libcurl\curl >nul 2>nul
-git clone --branch %CURL_TAG% --depth=1 https://github.com/curl/curl libcurl\curl >nul 2>nul
-
-if exist libcurl\curl (
-	cd libcurl\curl
-	cmd /c .\buildconf.bat >nul 2>nul
-	echo Initialization success!
-	exit 0
-)
-else (
-	echo Initialization failed: Network errors.
-	exit 1
-)
+git clone --branch curl-8_6_0 --depth=1 https://github.com/curl/curl libcurl\curl
+cmd /c libcurl\curl\buildconf.bat
