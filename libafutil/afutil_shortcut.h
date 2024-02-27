0382f264a96761ad75a6fcd5aa5579b89eaa6304
王太平 <3348544562@qq.com>
2024-02-27 21:59:07 +0800

    修复shortcut无法正常使用的bug

--- libafutil/afutil_shortcut.h
+++ libafutil/afutil_shortcut.h
@@ -8,8 +8,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
-	afutil_bool afutil_shortcut_create_multibyte(const char* source_file, const char* shortcut, const char* description, const char* working_dir, const char* icon_path, int icon_id = 0);
-	afutil_bool afutil_shortcut_create_wide(const wchar_t* source_file, const wchar_t* shortcut, const wchar_t* description, const wchar_t* working_dir, const wchar_t* icon_path, int icon_id = 0);
+	AFUTIL_EXPORT afutil_bool afutil_shortcut_create_multibyte(const char* source_file, const char* shortcut, const char* description, const char* working_dir, const char* icon_path, int icon_id = 0);
+	AFUTIL_EXPORT afutil_bool afutil_shortcut_create_wide(const wchar_t* source_file, const wchar_t* shortcut, const wchar_t* description, const wchar_t* working_dir, const wchar_t* icon_path, int icon_id = 0);
 #ifdef __cplusplus
 }
 #endif // __cplusplus
