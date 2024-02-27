76ab1a0fe734239d6e58cd6da780a7a096496fdc
王太平 <3348544562@qq.com>
2024-02-27 20:55:46 +0800

    更改curl仓库的拉取方式

--- curl-init.bat
+++ curl-init.bat
@@ -4,5 +4,5 @@ if not exist libafutil.sln (
 	exit 1
 )
 rmdir /s /q libcurl\curl >nul 2>nul
-git clone --branch curl-8_6_0 --depth=1 https://github.com/curl/curl.git libcurl\curl
+git clone --branch curl-8_6_0 --single-branch --depth=1 https://github.com/curl/curl.git libcurl\curl
 cmd /c libcurl\curl\buildconf.bat
